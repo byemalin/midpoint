574b6310075c08fef457a069205db37ac91d8a3f
Maksim Fomkin <fomaxxi@gmail.com>
2023-08-29 17:28:33 +0200

    roots and psudo code

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # Defines the root path route ("/")
   root to: "pages#home"
+
+  resources :meetups, only: [:show, :index, :destroy]
+
+  resources :destinations, only: [:show] do
+    resources :flights
+  end
+
 end
