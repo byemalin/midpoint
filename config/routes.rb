918418e86e175e18f7240e0b87c4237c764133e6
david graham <davidcgraham09@gmail.com>
2023-10-23 11:47:09 +0200

    Reinstated devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # devise_for :users
+  devise_for :users
   # Defines the root path route ("/")
   root to: "pages#home"
   resources :meetups, only: [:show, :create]
